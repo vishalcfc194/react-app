fc99a5954dbc831efcb865299aed4f619fe9ddc5
vishalcfc194 <140190926+vishalcfc194@users.noreply.github.com>
2023-12-18 16:39:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import First from './components/First';
+ 
 import Second from './components/Second';
 
 function App() {
