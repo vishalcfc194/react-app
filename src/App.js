f996aad91ad163c8096a9acbbe24ddec134aef7f
vishalcfc194 <vishalcode10@gmail.com>
2023-12-18 16:16:19 +0530

    second time commit

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Second />} />
-           
-        </Routes>
+         </Routes>
       </Router>
     </div>
   );
